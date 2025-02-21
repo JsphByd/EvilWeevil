6ba80b4c374201d96134b8370071d73be14f10d5
Joseph Boyd <jboyd@blackhillsinfosec.com>
2025-02-20 17:43:58 -0700

    init

--- src/test.go
+++ src/test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+
 	//"bufio"
 	//"bytes"
 	"flag"
@@ -20,8 +21,8 @@ type yamlData struct {
 	SearchTerms []string `yaml:"search_terms"`
 }
 
-type  Config struct{
-	Domain	 string
+type Config struct {
+	Domain   string
 	NetGraph map[string][]string
 }
 
@@ -49,31 +50,33 @@ func parser(graph Config, domain string) Config {
 	//var buf bytes.Buffer
 	var URL string
 	outgoingMap := make(map[string][]string)
-	
+
 	if !strings.Contains(domain, "http") {
 		URL = "https://" + domain
 	} else {
 		URL = domain
 	}
 	graph.Domain = URL
-	
+
 	responseBody := getRespBody(URL)
 	defer responseBody.Close()
 
 	// UU
 	//create a copy of resp body
-	//tee := io.TeeReader(urlData, &buf)
+	//tee := io.TeeReader(responseBody, &buf)
 	//bytes, _ := io.ReadAll(tee)
 	//siteString := string(bytes)
+
+	//fmt.Println(siteString)
 
 	outgoingMap[URL] = getOutgoingLinks(responseBody, URL)
 	graph.NetGraph = outgoingMap
 	/*
-	1. Get outgoing links
+		1. Get outgoing links
 	*/
-	for _, link := range(graph.NetGraph[URL]) {
-		parser(graph, link)
-	}
+	//for _, link := range graph.NetGraph[URL] {
+	//	parser(graph, link)
+	//}
 
 	return graph
 }
@@ -82,23 +85,28 @@ func getOutgoingLinks(htmlBody io.Reader, URL string) []string {
 	var outGoingLinks []string
 	doc, err := html.Parse(htmlBody)
 	if err != nil {
-		log.Fatal("KILLED ON URL",URL,"FOR ERROR",err)
+		log.Fatal("KILLED ON URL", URL, "FOR ERROR", err)
 	}
 
-	var f func(*html.Node, []string) []string
-	f = func(n *html.Node, linkMap []string) []string {
+	//var f func(*html.Node, []string) []string
+	var f func(*html.Node)
+	f = func(n *html.Node) {
 		if n.Type == html.ElementNode {
+			fmt.Println(n.Data)
 			if n.Data == "a" {
 				for _, attr := range n.Attr {
-					if attr.Key == "href" && attr.Val != "#" && string(attr.Val[0]) != "?" {
-						aPath := attr.Val
-						if !strings.HasPrefix(aPath, "http") {
-							if string(aPath[0]) == "/" { //rel link
-								aPath = URL + "/" + attr.Val
-							} else if strings.HasPrefix(aPath, "//") {
-								aPath = "https:" + attr.Val
-							} else {
-								aPath = URL + aPath //UU
+					if len(attr.Val) != 0 {
+						if attr.Key == "href" && attr.Val != "#" && string(attr.Val[0]) != "?" {
+							aPath := attr.Val
+							fmt.Println(aPath)
+							if !strings.HasPrefix(aPath, "http") {
+								if string(aPath[0]) == "/" { //rel link
+									aPath = URL + attr.Val
+								} else if strings.HasPrefix(aPath, "//") {
+									aPath = "https:" + attr.Val
+								} else {
+									aPath = URL + "/" + aPath //UU
+								}
 							}
 							outGoingLinks = append(outGoingLinks, aPath)
 						}
@@ -106,9 +114,12 @@ func getOutgoingLinks(htmlBody io.Reader, URL string) []string {
 				}
 			}
 		}
-		return linkMap
+
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
 	}
-	outGoingLinks = f(doc, outGoingLinks)
+	f(doc)
 	return outGoingLinks
 }
 
@@ -121,7 +132,6 @@ func getRespBody(URL string) io.ReadCloser {
 	}
 	return resp.Body
 }
-
 
 func readDomainsFile(path string) yamlData {
 	var config yamlData
